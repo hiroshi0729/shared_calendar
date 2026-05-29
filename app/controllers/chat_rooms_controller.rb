9780ce646662c15f797374903955de3e442919f2
hiroshi <yang08.higayodo729@gmail.com>
2026-05-29 22:33:56 +0900

    グールプチャットの作成

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -4,12 +4,9 @@ class ChatRoomsController < ApplicationController
 
   # チャットルーム一覧
   def index
-    @chat_rooms = ChatRoom.includes(:messages).order(created_at: :desc)
-  end
-
-  # チャットルーム作成フォーム（追加）
-  def new
-    @chat_room = ChatRoom.new
+    @chat_rooms = current_user.chat_rooms
+                               .includes(:users, :messages)
+                               .order(created_at: :desc)
   end
 
   # チャットルーム詳細
@@ -18,25 +15,54 @@ class ChatRoomsController < ApplicationController
     @message = Message.new
   end
 
+  # 個人チャット作成画面
+  def direct_new
+    @chat_room = ChatRoom.new  # ← 追加
+    # 自分以外のユーザーを取得
+    @users = User.where.not(id: current_user.id)
+  end
+
+  # グループチャット作成画面
+  def new
+    @chat_room = ChatRoom.new
+    # 自分以外のユーザーを取得
+    @users = User.where.not(id: current_user.id)
+  end
+
   # チャットルーム作成
   def create
     @chat_room = ChatRoom.new(chat_room_params)
-
+    
     if @chat_room.save
-      redirect_to @chat_room, notice: 'チャットルームを作成しました'
+      # 作成者をメンバーに追加
+      @chat_room.chat_room_memberships.create!(user: current_user)
+      
+      # 選択されたユーザーをメンバーに追加
+      if params[:user_ids].present?
+        params[:user_ids].each do |user_id|
+          @chat_room.chat_room_memberships.create!(user_id: user_id)
+        end
+      end
+      
+      redirect_to chat_room_path(@chat_room), success: 'チャットルームを作成しました'
     else
-      # エラー時は new.html.erb を再表示
-      render :new, status: :unprocessable_entity
+      if @chat_room.direct_message?
+        @users = User.where.not(id: current_user.id)
+        render :direct_new, status: :unprocessable_entity
+      else
+        @users = User.where.not(id: current_user.id)
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   private
 
   def set_chat_room
-    @chat_room = ChatRoom.find(params[:id])
+    @chat_room = current_user.chat_rooms.find(params[:id])
   end
 
   def chat_room_params
-    params.require(:chat_room).permit(:name)
+    params.require(:chat_room).permit(:name, :room_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   # チャット機能のルーティング
   resources :chat_rooms, only: [:index, :show, :new, :create] do
     resources :messages, only: [:create]
+    collection do
+      get :direct_new  # 個人チャット作成用
+    end
   end
 
   # プロフィール
